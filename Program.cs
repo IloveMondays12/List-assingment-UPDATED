a00daae0796baa3f0acb040c6d7c45614fbcd3b9
Ethan Jones <339467243@GoTVDSB.ca>
2024-04-18 12:05:32 -0400

    apr 18

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace List_assingment_UPDATED
+﻿using System.Runtime.InteropServices;
+
+namespace List_assingment_UPDATED
 {
     internal class Program
     {
@@ -6,49 +8,49 @@
         {
             Random generator = new Random();
             List<int> numRange = new List<int>();
-            int numCalled = 0, menuChoice, removeValue, addValue, interestNum;
-            int numCount = 0;
-            bool menu = false, validValue, advancedMenu;
+            int numCalled = 0, menuChoice, removeValue, addValue, interestNum, biggestNum,smallestNum;
+            int numCount = 0, numTotal = 0;
+            string menuTwoChoice;
+            bool menu = false, validValue, advancedMenu = false;
             
                 for (int t = 0; t < 25; t++)
                 {
                     numRange.Add(generator.Next(10, 21));
                 }
             Console.WriteLine("Here are your numbers:");
-            for (int i = 0; i < 5; i++)
-            {
-
+            
             while (!menu)
             {
+                Console.Clear();
+                for (int i = 0; i < ((numRange.Count / 5) + 1); i++)
+                {
+                    int u;
+                    if ((numRange.Count / 5) == i)
+                    {
+                        u = numRange.Count - (i * 5);
+                    }
+                    else
+                    {
+                        u = 5;
+                    }
+                    for (int t = 0; t < u; t++)
+                    {
+
+                        Console.Write(numRange[numCalled] + ", ");
+                        numCalled++;
+                    }
+                    Console.WriteLine();
+                }
                 numCalled = 0;
                 Console.WriteLine("1) Sort your List (lowest to highest)\n2) Refresh your numbers \n3) Remove a value \n4) Add a value \n5) How many times does a number appear? \n6) Largest value \n7) Smallest value \n8) Advanced Options \n9) QUIT");
                 Console.WriteLine("Write here what you want to explore first! ():");
                 if(int.TryParse(Console.ReadLine(), out menuChoice))
                 {
-                    Console.Clear();
                     if (menuChoice ==1)
                     {
                         Console.WriteLine("Here are your numbers in a new ordered fashion:");
                         numRange.Sort();
-                        for (int i = 0; i < ((numRange.Count / 5) + 1); i++)
-                        {
-                            int u;
-                            if ((numRange.Count / 5) == i)
-                            {
-                                u = numRange.Count - (i * 5);
-                            }
-                            else
-                            {
-                                u = 5;
-                            }
-                            for (int t = 0; t < u; t++)
-                            {
-
-                                Console.Write(numRange[numCalled] + ", ");
-                                numCalled++;
-                            }
-                            Console.WriteLine();
-                        }
+                        
                     }
                     else if (menuChoice == 2)
                     {
@@ -58,25 +60,7 @@
                             numRange.Add(generator.Next(10, 21));
                         }
                         Console.WriteLine("Your NEW numbers good sir:") ;
-                        for (int i = 0; i < ((numRange.Count / 5) + 1); i++)
-                        {
-                            int u;
-                            if ((numRange.Count / 5) == i)
-                            {
-                                u = numRange.Count - (i * 5);
-                            }
-                            else
-                            {
-                                u = 5;
-                            }
-                            for (int t = 0; t < u; t++)
-                            {
-
-                                Console.Write(numRange[numCalled] + ", ");
-                                numCalled++;
-                            }
-                            Console.WriteLine();
-                        }
+                        
 
                     }
                     else if (menuChoice == 3)
@@ -96,25 +80,7 @@
                                     }
                                 }
                                 Console.WriteLine("Let's take a look at your updated values:");
-                                for (int i = 0; i < ((numRange.Count / 5) + 1); i++)
-                                {
-                                    int u;
-                                    if ((numRange.Count / 5) == i)
-                                    {
-                                        u = numRange.Count - (i * 5);
-                                    }
-                                    else
-                                    {
-                                        u = 5;
-                                    }
-                                    for (int t = 0; t < u; t++)
-                                    {
-
-                                        Console.Write(numRange[numCalled] + ", ");
-                                        numCalled++;
-                                    }
-                                    Console.WriteLine();
-                                }
+                                
                                 validValue = true;
                             }
                             else
@@ -133,25 +99,7 @@
                             {
                                 numRange.Add(addValue);
                                 Console.WriteLine("Your new numbers:");
-                                for (int i = 0; i < ((numRange.Count / 5) + 1); i++)
-                                {
-                                    int u;
-                                    if ((numRange.Count / 5) == i)
-                                    {
-                                        u = numRange.Count - (i * 5);
-                                    }
-                                    else
-                                    {
-                                        u = 5;
-                                    }
-                                    for (int t = 0; t < u; t++)
-                                    {
-
-                                        Console.Write(numRange[numCalled] + ", ");
-                                        numCalled++;
-                                    }
-                                    Console.WriteLine();
-                                }
+                                
                                 validValue = true;
                             }
                             else
@@ -177,6 +125,7 @@
                                     }
                                 }
                                 Console.WriteLine($"\nNumber of '{interestNum}'s: {numCount}");
+                                interestNum = 0;
                                 Thread.Sleep(1000);
                                 validValue = true;
                             }
@@ -188,21 +137,63 @@
                     }
                     else if (menuChoice == 6)
                     {
-
+                        biggestNum = numRange[0];
+                        for (int i = 0; i < numRange.Count; i++)
+                        {
+                         if (biggestNum < numRange[i])
+                            {
+                                biggestNum = numRange[i];
+                            }
+                        }
+                     Console.WriteLine($"After looking over your list we've narrowed your biggest number to a lowly {biggestNum}");
                     }
                     else if (menuChoice == 7)
                     {
-
+                        smallestNum = numRange[0];
+                        for (int i = 0; i < numRange.Count; i++)
+                        {
+                            if (smallestNum > numRange[i])
+                            {
+                                smallestNum = numRange[i];
+                            }
+                        }
                     }
                     else if (menuChoice == 8)
                     {
+                       while (advancedMenu == false)
+                        {
+                            Console.WriteLine("In here you can find the three functions of data sets! \na) The mean (average)\nb) The median (middle number)\nc)The mode (most common number) \nd) Back to main menu");
+                        menuTwoChoice = Console.ReadLine().ToLower().Trim();
+                            if (menuTwoChoice == "a")
+                            {
+                                for (int i = 0; i < numRange.Count; i++)
+                                {
+                                    numTotal = numTotal + numRange[i];
+                                }
+                                Console.WriteLine($"The average of your numbers add up to be... {numTotal/numRange.Count} \nGood luck!");
+                            }
+                            if (menuTwoChoice == "b")
+                            {
+                                if (numRange.Count % 2 == 0)
+                                {
+                                    Console.WriteLine($"Your middle number is between {(numRange[numRange.Count/2]+ numRange[(numRange.Count / 2)+1])/2}");   
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Your median number is {(numRange[numRange.Count / 2] + numRange[(numRange.Count / 2) + 1]) / 2}");
+                                }
+                            }
+                            if (menuTwoChoice == "c")
+                            {
 
+                            }
+                        }
                     }
                     else if (menuChoice == 9)
                     {
-
+                        menu = true;
                     }
-                    Console.WriteLine("Press 'Enter' to continue back to the menu");
+                    Console.WriteLine("Press 'Enter' to continue.");
                     Console.ReadLine();
                 }
 
